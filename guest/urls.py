155c37599eb9607664d47ff22c29017255d09cc2
xhx1013 <1178108901@qq.com>
2019-05-06 09:14:01 +0800

    .

--- guest/urls.py
+++ guest/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
     url(r'^index/$', views.index),
-    url(r'^/accounts/login/$', views.index),
+    url(r'^/accounts/login/', views.index),
     url(r'^login_action/$', views.login_action),
     url(r'^event_manage/$', views.event_manage),
 ]
